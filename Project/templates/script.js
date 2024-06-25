bb656082b6bc97380f84f903ee6f3677b825dd41
Yuri <yurimfs30@gmail.com>
2024-06-25 14:13:10 -0300

    add README.md

--- Project/templates/script.js
+++ Project/templates/script.js
@@ -3,10 +3,11 @@ function toggleMode() {
   html.classList.toggle("light")
 
   const img = document.querySelector("#profile img")
-
   if (html.classList.contains("light")) {
     img.setAttribute("src", "/Project/assets/cafe.png")
+    img.setAttribute("alt", "avatar1")
   } else {
     img.setAttribute("src", "/Project/assets/carga-de-trabalho.png")
+    img.setAttribute("alt", "avatar2")
   }
 }
