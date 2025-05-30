6cac6ffd4d4518f545563225248f488e0eadf007
Yuri <yurimfs30@gmail.com>
2025-05-30 16:00:19 -0300

    feat: Visual adjustment

--- Project/templates/script.js
+++ Project/templates/script.js
@@ -1,13 +1,4 @@
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
-
-  const img = document.querySelector("#profile img")
-  if (html.classList.contains("light")) {
-    img.setAttribute("src", "/Project/assets/cafe.png")
-    img.setAttribute("alt", "avatar1")
-  } else {
-    img.setAttribute("src", "/Project/assets/carga-de-trabalho.png")
-    img.setAttribute("alt", "avatar2")
-  }
 }
